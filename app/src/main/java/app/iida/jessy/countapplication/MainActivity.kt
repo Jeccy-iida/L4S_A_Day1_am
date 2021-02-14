b24f295c2250f8bb000dedaf7fdeb40f494648bf
Jeccy-iida <drrr_drrr_4242@docomo.ne.jp>
2021-02-15 05:20:41 +0900

    Must変更

--- app/src/main/java/app/iida/jessy/countapplication/MainActivity.kt
+++ app/src/main/java/app/iida/jessy/countapplication/MainActivity.kt
@@ -1,45 +1,87 @@
 package app.iida.jessy.countapplication
 
+import android.media.AudioAttributes
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var mSoundPool : SoundPool
+    private  lateinit var mSoundID :Array<Int?>
+
+    private val mSoundResource = arrayOf(
+        R.raw.donsound,
+        R.raw.paasound
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        // valのほうがいいかも？ わかりました！
-        // varだと再代入できるけど、多分findViewByIdして
-        // val plusButton = findViewById<Button>(R.id.plusButton)
-      //  var plusButton = findViewById<Button>(R.id.plusButton)
-      //  var minusButton = findViewById<Button>(R.id.minusButton)
-      //  var clearButton = findViewById<Button>(R.id.clearButton)
-      //  var textView = findViewById<TextView>(R.id.textView)
-          val plusButton = findViewById<Button>(R.id.plusButton)
-          val minusButton = findViewById<Button>(R.id.minusButton)
-          val clearButton = findViewById<Button>(R.id.clearButton)
-          val textView = findViewById<TextView>(R.id.textView)
+        //(R.layout.activity_main)
 
-        var number: Int =0
 
-        textView.text = "0"
+        //テキストボタンで文字を出す　https://liginc.co.jp/489910
 
-        plusButton.setOnClickListener {
-            number = number + 1
-            textView.text = number.toString()
+        textView.text = ""
+
+        donButton.setOnClickListener {
+
+            textView.text = textView.text as String +  "ドン"
+
         }
 
-        minusButton.setOnClickListener {
-            number = number - 1
-            textView.text = number.toString()
+        paaButton.setOnClickListener {
+
+            textView.text = textView.text as String +  "パッ"
+
         }
 
-        clearButton.setOnClickListener {
-            number = 0
-            textView.text = number.toString()
+        clear.setOnClickListener {
+
+            textView.text = ""
+
         }
 
+    }
+
+    override fun onResume(){
+        super.onResume()
+
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+
+        mSoundPool = SoundPool.Builder()
+            .setAudioAttributes(audioAttributes)
+            .setMaxStreams(mSoundResource.size)
+            .build()
+
+        mSoundID = arrayOfNulls(mSoundResource.size)
+
+        for(i in 0..(mSoundResource.size - 1)){
+            mSoundID[i] = mSoundPool.load(applicationContext,mSoundResource[i],0)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mSoundPool.release()
+    }
+
+    fun don(view: View){
+        if(mSoundID[0] != null){
+            mSoundPool.play(mSoundID[0] as Int,1.0F,1.0F,0,0,1.0F)
+        }
+
+    }
+    fun paaSound(view:View){
+        if(mSoundID[1] != null){
+            mSoundPool.play(mSoundID[1] as Int,1.0F,1.0F,0,0,1.0F)
+        }
     }
 }
